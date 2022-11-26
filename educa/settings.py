5789d68b209c70d4391941f0f2c95ed7418c911b
mj-creatives <mjcreativesllc@gmail.com>
2022-11-26 00:09:16 -0500

    FC

--- educa/settings.py
+++ educa/settings.py
@@ -277,6 +277,8 @@ if DEBUG == False:
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_LOCATION = 'static'
+    AWS_QUERYSTRING_AUTH = False
+    AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
