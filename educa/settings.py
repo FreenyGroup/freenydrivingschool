4ca7edd5dda477956ef346e9a29afc506fd9a42c
mj-creatives <mjcreativesllc@gmail.com>
2022-11-26 02:33:14 -0500

    Connecting to memcache.

--- educa/settings.py
+++ educa/settings.py
@@ -293,38 +293,20 @@ if DEBUG == False:
 def get_cache():
   import os
   try:
-    servers = config('MEMCACHIER_SERVERS')
-    username = config('MEMCACHIER_USERNAME')
-    password = config('MEMCACHIER_PASSWORD')
+    servers = os.environ['MEMCACHIER_SERVERS']
+    username = os.environ['MEMCACHIER_USERNAME']
+    password = os.environ['MEMCACHIER_PASSWORD']
     return {
       'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
         # TIMEOUT is not the connection timeout! It's the default expiration
         # timeout that should be applied to keys! Setting it to `None`
         # disables expiration.
         'TIMEOUT': None,
         'LOCATION': servers,
         'OPTIONS': {
-          'binary': True,
           'username': username,
           'password': password,
-          'behaviors': {
-            # Enable faster IO
-            'no_block': True,
-            'tcp_nodelay': True,
-            # Keep connection alive
-            'tcp_keepalive': True,
-            # Timeout settings
-            'connect_timeout': 2000, # ms
-            'send_timeout': 750 * 1000, # us
-            'receive_timeout': 750 * 1000, # us
-            '_poll_timeout': 2000, # ms
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
-          }
         }
       }
     }
