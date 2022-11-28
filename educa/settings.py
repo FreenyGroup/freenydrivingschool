2737be5e4370e969afda98e6ad3bcdb7f171af26
mj-creatives <mjcreativesllc@gmail.com>
2022-11-28 13:08:12 -0500

    Edit AWS

--- educa/settings.py
+++ educa/settings.py
@@ -290,10 +290,9 @@ if DEBUG == False:
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_LOCATION = 'static'
+    #AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_QUERYSTRING_AUTH = False
-    AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
+    #AWS_S3_OBJECT_PARAMETERS = {'Access-Control-Allow-Origin': '*'}
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
