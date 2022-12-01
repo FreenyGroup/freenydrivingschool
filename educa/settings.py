4a5f5016dfc4f99b443a094ab48d20097a61acc4
mj-creatives <mjcreativesllc@gmail.com>
2022-12-01 14:32:25 -0500

    AWS OVERWRITE

--- educa/settings.py
+++ educa/settings.py
@@ -241,6 +241,7 @@ if DEBUG == False:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_QUERYSTRING_AUTH = False
     AWS_DEFAULT_ACL='public-read'
+    AWS_S3_FILE_OVERWRITE = False
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
